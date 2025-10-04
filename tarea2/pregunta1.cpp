ce30c321d39969ea69e7a1962735500278ee0d07
A6575 <astridaalvaradoa@gmail.com>
2025-10-04 09:32:47 -0400

    Agregada documentación al programa

--- tarea2/pregunta1.cpp
+++ tarea2/pregunta1.cpp
@@ -1,3 +1,35 @@
+/* 
+    Implementación de pseudo-código propuesto.
+
+ * Detalles de implementación:
+      - En lugar de tener una estructura especial "Canciones", se decidió trabajar con 3 vectores:
+            + init: vector para almacenar el tiempo en que inician cada canción
+            + duration: vector para almacenar la duración de cada canción
+            + finish: vector de pares para almacenar el tiempo de finalización y el id de cada canción.
+        En estos vectores, el id de las canciones serán representadas por los índices del arreglo, por
+        lo que la canción i será equivalente al índice i.
+
+      - Se ordena el vector de pares por tiempo de finalización.
+
+      - Se selecciona la primera canción y se compara el tiempo de inicio de las siguientes
+      canciones con el tiempo de finalización de la última canción seleccionada.
+
+      - Si el tiempo de inicio de la canción actual es mayor o igual al tiempo de
+        finalización de la última canción seleccionada, se agrega a la lista de canciones seleccionadas.
+
+      - Se devuelve el vector de índices de las canciones seleccionadas.
+
+ * Ejemplo de uso:
+      - Se define un vector de tiempos de inicio y otro de duraciones de canciones. En el índice 0 de cada
+        vector se coloca -1 como valor dummie para asegurar un correcto comportamiento del algoritmo.
+
+      - Se llama a la función `select_songs` con estos vectores.
+
+      - Se imprime el resultado de las canciones seleccionadas.
+
+      - El resultado es un vector de índices de las canciones seleccionadas.
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -28,11 +60,11 @@ vector<int> select_songs(vector<int> init, vector<int> duration){
 }
 
 int main() {
-    //Canciones                1  2  3  4
-    vector<int> init     = {0, 1, 2, 1, 7};
-    vector<int> duration = {0, 5, 4, 1, 2};
+    //Canciones                 1  2  3
+    vector<int> init     = {-1, 1, 2, 3};
+    vector<int> duration = {-1, 4, 1, 3};
     vector<int> selected = select_songs(init, duration);
-    cout << "Selected songs: {";
+    cout << "Selected songs: { ";
     for (int i : selected) {
         cout << i << " ";
     }
